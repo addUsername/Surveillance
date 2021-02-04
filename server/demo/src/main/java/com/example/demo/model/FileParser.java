5f3f9b519783268c62ff5e28f95aa29cfd68c060
addUsername <60299373+addUsername@users.noreply.github.com>
2021-02-04 23:43:39 +0100

    Add files via upload

--- server/demo/src/main/java/com/example/demo/model/FileParser.java
+++ server/demo/src/main/java/com/example/demo/model/FileParser.java
@@ -5,11 +5,15 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.concurrent.SynchronousQueue;
 
 import org.springframework.stereotype.Component;
+import org.springframework.util.Base64Utils;
 /**
  * Model is gonna be a lot more complex:
  * - User.class
@@ -24,26 +28,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class FileParser {
 
-	private SynchronousQueue<byte[]> buffer;
+	private HashMap<Integer,SynchronousQueue<byte[]>> videos;
+	
 	private BufferedOutputStream bos;
 	private String path = "video";
 	private final String MJPEG = ".mjpg";
 	private final String H264 = ".h264";
-	private int id;
 	
 	public FileParser() {
 		System.err.println("file parse instance created");
+		this.videos= new HashMap<Integer, SynchronousQueue<byte[]>>();
 	}
 	
-	public void add(byte[] fragment) {
+	public void add(byte[] fragment, int videoId) {
 		try {
-			buffer.put(fragment);
+			videos.get(videoId).put(fragment);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-
+	/*
 	public boolean save() {
 		try {
 			bos = new BufferedOutputStream(new FileOutputStream(id+path)); 
@@ -57,19 +62,19 @@ public class FileParser {
 		}
 		return new File(path).exists();
 	}
-
-	public void iniUpload(int id) {
-		buffer = new SynchronousQueue<byte[]>();
-		this.id = id;		
+	*/
+	public void iniUpload(int id) {		
+		videos.put(id, new SynchronousQueue<byte[]>());
 	}
 	
 	public void writeStream(OutputStream responseOs, int videoId, String type) {
 		
 		if(type.equals(H264)) {
-			writeStreamH264(responseOs);
+			writeStreamH264(responseOs, videoId);
 			return;
 		}
 		byte[] bytes = null;
+		SynchronousQueue<byte[]> buffer = videos.get(videoId);
 		while(true) {			
 			try {
 				bytes = buffer.take();
@@ -91,16 +96,17 @@ public class FileParser {
 		}		
 	}
 
-	private void writeStreamH264(OutputStream responseOs) {
+	private void writeStreamH264(OutputStream responseOs, int videoId) {
 		
 		byte[] bytes = null;
+		SynchronousQueue<byte[]> buffer = videos.get(videoId);
 		while(true) {			
 			try {
 				bytes = buffer.take();
 				if (bytes == null) return;
 				responseOs.write(bytes);
 				responseOs.flush();
-				Thread.sleep(1000/20);
+				Thread.sleep(1000/10);
 			} catch (IOException | InterruptedException e) {
 				// TODO Auto-generated catch block
 				//e.printStackTrace();
