c7c5c836c9aeb7afa7f76137706c4c8af9978910
addUsername <60299373+addUsername@users.noreply.github.com>
2021-02-18 23:45:06 +0100

    Add files via upload

--- server/demo/src/main/java/com/example/demo/configs/WebSocketConfig.java
+++ server/demo/src/main/java/com/example/demo/configs/WebSocketConfig.java
@@ -10,8 +10,18 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
  * TODO allow binary data, 30% on size reduction, check:
  * Rsocket, multipart request, streaming request
  * @author SERGI
- *
  */
+
+ /**
+  * A litle bit of theory https://www.baeldung.com/spring-websockets-send-message-to-user and question +2 from https://stackoverflow.com/questions/22367223/sending-message-to-specific-user-on-spring-websocket
+  
+There are three main ways to say where messages are sent and how they are subscribed to using Spring WebSockets and STOMP:
+Topics – common conversations or chat topics open to any client or user
+Queues – reserved for specific users and their current sessions
+Endpoints – generic endpoints
+
+ And when your client subscribe to an channel start with /user/, eg: /user/queue/reply, your server instance will subscribe to a queue named queue/reply-user[session id]
+  */
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
@@ -19,7 +29,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	  public void configureMessageBroker(MessageBrokerRegistry config) {
-	    config.enableSimpleBroker("/topic");
+	    config.enableSimpleBroker("/topic");//("/topic" , "/queue");
+
+		// on client side subscribe("/{id}/queue/reply").. 
+		// on controller simpMessagingTemplate.convertAndSendToUser(id_raspberry, "/queue/reply", message); // o el fichero settings.properties.. gut
 	    config.setApplicationDestinationPrefixes("/app");
 	  }
 	
