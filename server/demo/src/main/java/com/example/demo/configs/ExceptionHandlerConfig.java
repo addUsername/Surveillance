1b7a048b70568f6eb10951120a441772f0633440
addUsername <60299373+addUsername@users.noreply.github.com>
2021-03-03 19:33:32 +0100

    Add files via upload

---
+++ server/demo/src/main/java/com/example/demo/configs/ExceptionHandlerConfig.java
@@ -0,0 +1,50 @@
+package com.example.demo.configs;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.example.demo.exceptions.BadDumpException;
+import com.example.demo.exceptions.BadLoginException;
+import com.example.demo.exceptions.UserAlreadyExistsException;
+
+@RestControllerAdvice
+public class ExceptionHandlerConfig extends ResponseEntityExceptionHandler {
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		Map<String, String> errors = new HashMap<>();
+		
+		ex.getBindingResult().getAllErrors().forEach((error) ->{
+			
+			String fieldName = ((FieldError) error).getField();
+			String message = error.getDefaultMessage();
+			errors.put(fieldName, message);
+		});
+		return new ResponseEntity<Object>(errors, HttpStatus.BAD_REQUEST);
+	}
+	/*
+	 * Good place to call logger..
+	 */
+	@ExceptionHandler({BadDumpException.class, BadLoginException.class, UserAlreadyExistsException.class})
+	public ResponseEntity<Object> handleBadDumpException(Exception ex,  WebRequest request){		
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now().toString());
+        body.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+	}
+	
+}
