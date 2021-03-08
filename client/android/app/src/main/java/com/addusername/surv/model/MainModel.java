d77ea3ca9b100c75ac5e48b69df113213449b6dd
addUsername <60299373+addUsername@users.noreply.github.com>
2021-03-08 16:47:52 +0100

    finally working

--- client/android/app/src/main/java/com/addusername/surv/model/MainModel.java
+++ client/android/app/src/main/java/com/addusername/surv/model/MainModel.java
@@ -7,22 +7,27 @@ import com.addusername.surv.dtos.RegisterForm;
 import com.addusername.surv.interfaces.ModelOps;
 import com.addusername.surv.interfaces.PresenterOpsModel;
 
-import org.json.JSONException;
+import org.apache.commons.io.FileUtils;
 import org.json.JSONObject;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Field;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -32,19 +37,15 @@ public class MainModel implements ModelOps {
 
     private PresenterOpsModel pom;
     private final ExecutorService bgExecutor = Executors.newSingleThreadExecutor();
-    private RestTemplate rt = new RestTemplate();
+    private RestTemplate rt;
     private String FILESDIR;
-    private final String FILESQL = "dump.sql";
+    private final String FILESQL = "dump2.sql";
 
     public MainModel(PresenterOpsModel pom, File file) {
 
         this.pom = pom;
         this.FILESDIR = file.getAbsolutePath();
-    }
-
-    @Override
-    public boolean existsUser() {
-        return new File(FILESDIR +"/"+ FILESQL).exists();
+        this.rt = new RestTemplate();
     }
 
     @Override
@@ -54,6 +55,40 @@ public class MainModel implements ModelOps {
             @Override
             public void run() {
 
+                MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+
+                Resource content = null;
+                try {
+                    content = new MultipartByteArrayResource(FileUtils.readFileToByteArray(new File(FILESDIR +"/"+ FILESQL)), FILESQL);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                // dump.sql
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+                HttpEntity<Resource> requestEntityBody = new HttpEntity<Resource>(content, headers);
+
+                // pinDTO
+                HttpHeaders head = new HttpHeaders();
+                head.setContentType(MediaType.APPLICATION_JSON);
+                HttpEntity<String> pinEntity = new HttpEntity<>("{ \"pin\":"+loginForm.getPin()+"}",head);
+
+                body.add("data", requestEntityBody);
+                body.add("pin", pinEntity);
+
+                HttpHeaders requestHeaders = new HttpHeaders();
+                requestHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
+                HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, requestHeaders);
+                ResponseEntity<String> response = null;
+                try {
+
+                    response = rt.exchange("http://192.168.1.51:8080/auth/login", HttpMethod.POST, requestEntity, String.class);
+
+                    Log.d("Response:", response.toString());
+                } catch (Exception exception) {
+                    Log.d("loging", exception.getMessage());
+                }
             }
         });
     }
@@ -93,7 +128,6 @@ public class MainModel implements ModelOps {
                     e.printStackTrace();
                 }
                 Log.d("reg","exist file: "+existsUser());
-
             }
         });
     }
@@ -103,5 +137,28 @@ public class MainModel implements ModelOps {
 
         List<String> list = RegisterFormValidator.getErrors(rf);
         return (list.size() > 0)? list.toArray(new String[list.size()]) : null;
+    }
+
+    public class MultipartByteArrayResource extends ByteArrayResource{
+
+        private String fileName;
+
+        public MultipartByteArrayResource(byte[] byteArray, String filename) {
+            super(byteArray);
+            this.fileName = filename;
+        }
+
+        public String getFilename() {
+            return fileName;
+        }
+
+        public void setFilename(String fileName) {
+            this.fileName= fileName;
+        }
+    }
+    
+    @Override
+    public boolean existsUser() {
+        return new File(FILESDIR +"/"+ FILESQL).exists();
     }
 }
